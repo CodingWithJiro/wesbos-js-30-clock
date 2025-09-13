6e6cf89d77862bdf2151e9ec7cc8cef9074484d4
CodingWithJiro <chavezelmar03@gmail.com>
2025-09-14 00:40:20 +0800

    feat(js): add current seconds hand logic

--- js/modules/clock.js
+++ js/modules/clock.js
@@ -2,5 +2,25 @@
 const hour = document.querySelector(".clock__hour");
 const minute = document.querySelector(".clock__minute");
 const second = document.querySelector(".clock__second");
+const dateNow = new Date();
+let secondDegrees = dateNow.getSeconds() * 6;
 
-export function initClock() {}
+function rotateSecondsHand() {
+  setInterval(() => {
+    console.log(secondDegrees);
+    if (secondDegrees === 354) {
+      second.style.transform = "none";
+      secondDegrees = 0;
+    } else {
+      second.style.transform =
+        "transform 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)";
+      secondDegrees += 6;
+    }
+
+    second.style.transform = `translate(-50%, -100%) rotate(${secondDegrees}deg)`;
+  }, 1000);
+}
+
+export function initClock() {
+  rotateSecondsHand();
+}
