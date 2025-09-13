acf0fdb23b8c7623854f1c594536028b35821e2e
CodingWithJiro <chavezelmar03@gmail.com>
2025-09-13 22:23:59 +0800

    feat(js): select second, minute, and hour hand elements

--- js/modules/clock.js
+++ js/modules/clock.js
@@ -1,3 +1,6 @@
 //* CLOCK.JS SCRIPT
+const hour = document.querySelector(".clock__hour");
+const minute = document.querySelector(".clock__minute");
+const second = document.querySelector(".clock__second");
 
 export function initClock() {}
