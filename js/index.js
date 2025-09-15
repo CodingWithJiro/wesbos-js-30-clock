1caef45d456fc961c3e23928637d7abd4348b1af
CodingWithJiro <chavezelmar03@gmail.com>
2025-09-15 22:38:41 +0800

    feat(js): add location.js main logic

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // * IMPORT MODULES
 import { initThemeToggle } from "./modules/theme.js";
 import { initClock } from "./modules/clock.js";
+import { initLocation } from "./modules/location.js";
 
 // * INITIALIZE SCRIPTS
 initThemeToggle();
 initClock();
+initLocation();
