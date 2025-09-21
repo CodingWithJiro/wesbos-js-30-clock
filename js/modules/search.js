7aa45396bcb1eff5dbbabeda79fa423214d99084
CodingWithJiro <chavezelmar03@gmail.com>
2025-09-21 16:28:43 +0800

    feat(js): add load zones function and cache all country and zone name

--- js/modules/search.js
+++ js/modules/search.js
@@ -2,3 +2,12 @@
 import { getCountryData } from "./api.js";
 
 // * SEARCH.JS SCRIPT
+const cachedZones = [];
+
+async function loadZones() {
+  const { zones } = await getCountryData();
+
+  for (const { countryName, zoneName } of zones) {
+    cachedZones.push({ countryName: countryName, zoneName: zoneName });
+  }
+}
