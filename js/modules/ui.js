e4bc121d41877ce2fa8bd8f2ffbd33f2d172a05c
CodingWithJiro <chavezelmar03@gmail.com>
2025-09-27 15:12:24 +0800

    feat(js): select clock hand elements from dom

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -1,1 +1,4 @@
 // * UI.JS SCRIPT
+const hour = document.querySelector(".clock__hour");
+const minute = document.querySelector(".clock__minute");
+const second = document.querySelector(".clock__second");
