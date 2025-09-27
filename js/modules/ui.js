873703359f708a1684345c20b95befeab806eb04
CodingWithJiro <chavezelmar03@gmail.com>
2025-09-27 16:34:40 +0800

    refactor(js): remove unused clock hand elements in ui.js

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -1,4 +1,1 @@
 // * UI.JS SCRIPT
-const hour = document.querySelector(".clock__hour");
-const minute = document.querySelector(".clock__minute");
-const second = document.querySelector(".clock__second");
